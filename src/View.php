6044bd1f8dde88b873bacef609c17080dc0f0b77
Muhammad Miftah Firdaus <muh.miftah.firdaus@gmail.com>
2022-11-03 16:36:41 +0700

    extract array model on view

--- src/View.php
+++ src/View.php
@@ -4,6 +4,7 @@ namespace Src;
 
 class View {
     public static function render(string $view, $model) {
+        $model = extract($model);
         require __DIR__ . "/../resources/views/{$view}.php";
     }
 }
