5f02b96de6cb6b5707125db64b45b34104d1eae4
Muhammad Miftah Firdaus <muh.miftah.firdaus@gmail.com>
2022-11-03 17:07:49 +0700

    blade templating

--- src/View.php
+++ src/View.php
@@ -2,9 +2,13 @@
 
 namespace Src;
 
+use Jenssegers\Blade\Blade;
+
 class View {
-    public static function render(string $view, $model) {
-        $model = extract($model);
-        require __DIR__ . "/../resources/views/{$view}.php";
+    public static function render(string $view, array $model) {
+        $views = __DIR__ . '/../resources/views';
+        $cache = __DIR__ . '/../resources/cache';
+        $blade = new Blade($views, $cache);
+        echo $blade->render($view, $model);
     }
 }
